75236d3b280f0e6d50d3ab2a15312c33edc74908
irene-fagnani <irene.fagnani@mail.polimi.it>
2024-11-29 16:05:38 +0000

    fix the inplace operations error

--- model.py
+++ model.py
@@ -246,6 +246,7 @@ class MD_multi(nn.Module):
     # update G, Ec
     self.enc_c_opt.zero_grad()
     self.gen_opt.zero_grad()
+    self.forward()
     self.backward_G_alone()
     self.enc_c_opt.step()
     self.gen_opt.step()
