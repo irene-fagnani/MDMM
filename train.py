0865fa40f5c3562431c5aaca7b34a101ad41ce44
irene-fagnani <irene.fagnani@mail.polimi.it>
2024-12-09 14:29:23 +0000

    saving the loss

--- train.py
+++ train.py
@@ -13,7 +13,12 @@ def main():
   print('\n--- load dataset ---')
   dataset = dataset_multi(opts)
   train_loader = torch.utils.data.DataLoader(dataset, batch_size=opts.batch_size, shuffle=True, num_workers=opts.nThreads)
-
+  # losses dictionary
+  losses = {
+    "loss_D_content": [],
+    "loss_D": [],
+    "loss_G": []
+}
   # model
   print('\n--- load model ---')
   model = MD_multi(opts)
@@ -44,12 +49,12 @@ def main():
 
       # input data
       # NVIDIA
-      images = images.cuda(opts.gpu).detach()
-      c_org = c_org.cuda(opts.gpu).detach()
+      #images = images.cuda(opts.gpu).detach()
+      #c_org = c_org.cuda(opts.gpu).detach()
       #print("c",c_org)
       #print("i",images.size())
-      #images = images.cpu().detach()
-      #c_org = c_org.cpu().detach()
+      images = images.cpu().detach()
+      c_org = c_org.cpu().detach()
       #c_trg = c_trg.cuda(opts.gpu).detach()
       #input()
       
@@ -58,17 +63,26 @@ def main():
       if opts.isDcontent:
         if (it + 1) % opts.d_iter != 0 and it < len(train_loader) - 2:
           model.update_D_content(images, c_org)
+          losses["loss_D_content"].append(model.disContent_loss)
+          print("loss_D_content", model.disContent_loss)
           continue
         else:
           model.update_D(images, c_org)
+          losses["loss_D"].append(model.loss_D)
+          print("loss_D", model.loss_D)
           model.update_EG()
+          losses["loss_G"].append(model.loss_G)
+          print("loss_G", model.loss_G)
       else:
         model.update_D(images, c_org)
+        losses["loss_D"].append(model.loss_D)
+        print("loss_D", model.loss_D)
         model.update_EG()
+        losses["loss_G"].append(model.loss_G)
+        print("loss_G", model.loss_G)
       # save to display file
       if not opts.no_display_img:
         saver.write_display(total_it, model)
-
       print('total_it: %d (ep %d, it %d), lr %08f' % (total_it, ep, it, model.gen_opt.param_groups[0]['lr']))
       total_it += 1
       if total_it >= max_it:
@@ -85,6 +99,20 @@ def main():
 
     # Save network weights
     saver.write_model(ep, total_it, model)
+    
+    import matplotlib.pyplot as plt
+
+  # Plot each loss
+  for key, value in losses.items():
+      plt.figure(figsize=(10, 5))
+      plt.plot(value, label=key)
+      plt.title(f"Loss Curve: {key}")
+      plt.xlabel("Iteration")
+      plt.ylabel("Loss")
+      plt.legend()
+      plt.grid()
+      plt.show()
+      plt.savefig(f"loss_{key}.png")
 
   return
 
