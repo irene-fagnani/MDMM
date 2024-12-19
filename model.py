fb9b9b318a3ee0a9739e6ba70fc6627244a92da9
Greta <greta.gorbani@mail.polimi.it>
2024-12-19 17:07:34 +0000

    KL loss

--- model.py
+++ model.py
@@ -312,8 +312,10 @@ class MD_multi(nn.Module):
 
     # KL loss - z_a
     if self.concat:
-      kl_element = self.mu.pow(2).add_(self.logvar.exp()).mul_(-1).add_(1).add_(self.logvar)
-      loss_kl_za = torch.sum(kl_element).mul_(-0.5) * 0.01
+      # kl_element = self.mu.pow(2).add_(self.logvar.exp()).mul_(-1).add_(1).add_(self.logvar)
+      # loss_kl_za = torch.sum(kl_element).mul_(-0.5) * 0.01
+      loss=GMVAE.LossFunctions()
+      loss_kl_za = loss.gaussian_loss(self.z_attr, self.mu, self.logvar, self.mu_recon, self.logvar_recon)
     else:
       loss_kl_za = self._l2_regularize(self.z_attr) * 0.01
 
